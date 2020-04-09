9104cdf62c94f16d3a7c66426a42cbbd5e60a7b4
Masafumi24 <61171572+Masafumi24@users.noreply.github.com>
2020-04-09 23:13:23 +0900

    category JS OK

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,5 @@ class Item < ApplicationRecord
   validates :images, presence: true
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images
+  belongs_to :category
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root to: "items#index"
-  resources :items
+  resources :items do
+    collection do
+      get 'get_category_children', defaults: { format: 'json' }
+      get 'get_category_grandchildren', defaults: { format: 'json' }
+    end
+  end
 end
