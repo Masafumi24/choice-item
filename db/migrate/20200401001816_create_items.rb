8952a7646efc102a360153439327aec348caf249
Masafumi24 <61171572+Masafumi24@users.noreply.github.com>
2020-04-09 22:35:20 +0900

    category seed OK

--- db/migrate/20200401001816_create_items.rb
+++ db/migrate/20200401001816_create_items.rb
@@ -2,6 +2,7 @@ class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
       t.string :name, null: false
+      t.references :category, foreign_key: {to_tabel: :categories}
       t.timestamps
     end
   end
