fd87ae46ff5d38e8d20dd9b9a072503ba755cd5e
Masafumi24 <61171572+Masafumi24@users.noreply.github.com>
2020-04-13 14:46:11 +0900

    INDEX if else present

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -13,3 +13,27 @@ document.addEventListener(
   },
   false
 );
+// document.addEventListener(
+//   "DOMContentLoaded", e => {
+//     let modal_open = document.getElementById("modalOpenBtn");
+//     modal_open.onclick = function () {
+//       $('#overlay').fadeIn();
+//       document.getElementById('modalCloseBtn').onclick = function () {
+//         $('#overlay').fadeOut();
+//       };
+//       document.getElementById("randomReturnBtn").onclick = function (){
+//         console.log("Yeah");
+//         $('#overlay').fadeOut( function() {
+//           (window.onload = function() {
+//             $('#overlay').fadeIn();
+//           })();
+//         });
+//       };
+//     };
+//   },
+//   false
+// );
+
+// // (window.onload = function() {
+// //   restore_scroll();
+// // })();
