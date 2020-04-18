aee54faafb9cafd3293c99b436b8ca21b36f6567
Masafumi24 <61171572+Masafumi24@users.noreply.github.com>
2020-04-18 12:26:11 +0900

    OK migrate

--- db/migrate/20200401001816_create_items.rb
+++ db/migrate/20200401001816_create_items.rb
@@ -4,6 +4,12 @@ class CreateItems < ActiveRecord::Migration[5.2]
       t.string :name, null: false
       t.references :category, foreign_key: {to_tabel: :categories}
       t.references :user, foreign_key: true
+      t.string :brand
+      t.text :status
+      t.integer :spring_id
+      t.integer :summer_id
+      t.integer :autumn_id
+      t.integer :winter_id
       t.timestamps
     end
   end
---
+++ db/migrate/20200418032303_create_coordinations.rb
@@ -0,0 +1,14 @@
+class CreateCoordinations < ActiveRecord::Migration[5.2]
+  def change
+    create_table :coordinations do |t|
+      t.string :name
+      t.string :image
+      t.string :brand
+      t.integer :spring_id
+      t.integer :summer_id
+      t.integer :autumn_id
+      t.integer :winter_id
+      t.timestamps
+    end
+  end
+end
