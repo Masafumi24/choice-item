94ae0143f7cb07e868ee60d0fdc273495d507798
Masafumi24 <61171572+Masafumi24@users.noreply.github.com>
2020-04-26 03:04:19 +0900

    Update image_uploader.rb

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -11,10 +11,6 @@ class ImageUploader < CarrierWave::Uploader::Base
  
   # 保存形式をJPGにする
 
-  def extension_white_list
-    %w(jpg jpeg gif png)
-  end
-
   def filename
     super.chomp(File.extname(super)) + '.jpg' if original_filename.present?
   end
@@ -24,8 +20,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  storage :fog
+  # storage :file
+  # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -33,11 +29,11 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  protected
-  def secure_token
-    var = :"@#{mounted_as}_secure_token"
-    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
-  end
+  # protected
+  # def secure_token
+  #   var = :"@#{mounted_as}_secure_token"
+  #   model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
+  # end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
@@ -61,9 +57,9 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
