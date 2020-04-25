d65b2b6b39f0234ea4d1386ac54390977ce37448
Masafumi24 <61171572+Masafumi24@users.noreply.github.com>
2020-04-26 04:47:29 +0900

    image OK

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,18 +4,33 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include Piet::CarrierWaveExtension
   # include CarrierWave::MiniMagick
   # 画像の上限を640x480にする
+  process :resize_to_limit => [700, 700]
+
   process :convert => 'jpg'
-  process :resize_to_limit => [10, 10]
   # process optimize: [quality: 50]
   # 保存形式をJPGにする
 
-  # def filename
-  #   super.chomp(File.extname(super)) + '.jpg' if original_filename.present?
-  # end
+  def extension_whitelist
+    %w(jpg jpeg gif png HEIC HEIF heic heif)
+  end
 
-  # def filename
-  #   "#{secure_token}.#{file.extension}" if original_filename.present?
-  # end
+
+  def filename
+    super.chomp(File.extname(super)) + '.jpg' if original_filename.present?
+  end
+ 
+ # ファイル名を日付にするとタイミングのせいでサムネイル名がずれる
+ #ファイル名はランダムで一意になる
+  def filename
+    "#{secure_token}.#{file.extension}" if original_filename.present?
+  end
+ 
+  protected
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
+  end
+
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
@@ -23,9 +38,9 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+  # def store_dir
+  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  # end
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
@@ -48,9 +63,6 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  def extension_whitelist
-    %w(jpg jpeg gif png)
-  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
@@ -63,11 +75,5 @@ class ImageUploader < CarrierWave::Uploader::Base
     storage :fog
   end
   
-  # protected
-  # def secure_token
-  #   var = :"@#{mounted_as}_secure_token"
-  #   model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
-  # end
-
   
 end
