d4ea071edcd92c0dcc98c77e9cd5cc02b0eb1148
Masafumi24 <61171572+Masafumi24@users.noreply.github.com>
2020-05-05 23:03:28 +0900

    search includes_images

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -92,7 +92,7 @@ class ItemsController < ApplicationController
   end
 
   def search
-    @items = Item.search(params[:keyword])
+    @items = Item.search(params[:keyword]).includes_images
   end
 
   private
