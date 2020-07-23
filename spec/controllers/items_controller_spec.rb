59dbcea184ba56d664c21fa944a30a9a2a85eba3
Masafumi24 <61171572+Masafumi24@users.noreply.github.com>
2020-07-23 11:46:24 +0900

    controller test

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 describe ItemsController do
+
+  describe 'GET #index' do
+    it "@items true data includes?" do
+      items = create_list(:item, 3)
+      get :index
+      expect(assigns(:items)).to match(items)
+    end
+  end
   
   describe 'GET #new' do
     it "redirect to new.html.haml?" do
@@ -8,4 +16,19 @@ describe ItemsController do
       expect(response).to render_template :new
     end
   end
+
+  # describe 'GET #edit' do
+  #   it "@item true data include?" do
+  #     item = create(:item)
+  #     get :edit, params: { id: item }
+  #     expect(assigns(:item)).to eq item
+  #   end
+
+  #   it "redirect to edit.html.haml?" do
+  #     item = create(:item)
+  #     get :edit, params: { id: item }
+  #     expect(response).to render_template :edit
+  #   end
+  # end
+
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,10 +2,10 @@ FactoryBot.define do
   
   factory :user do
     nickname {"こんちゃん"}
-    email {"testkonchan@gmail.com"}
     image {"test.jpeg"}
     password {"12345678"}
     password_confirmation {"12345678"}
+    sequence(:email) {Faker::Internet.email}
   end
 
 end
